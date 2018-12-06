d4cac3907715ef19964f73a9ccf92ac3e1982497
Thomas von Rosenberg <vonrosenberg.thomas@gmail.com>
2018-12-06 20:40:24 +0100

    [#3] Add simple bar chart

---
+++ src/components/CompoundInterestChart.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from 'recharts';
+
+export type SavingsPlan = {
+    monthlyInvestment: number;
+    interestRate: number;
+    durationInYears: number;
+};
+
+type BarEntry = {return: number; investment: number; year: string};
+const range: (a: number, b: number) => number[] = (a: number, b: number) => {
+    const result: number[] = [];
+    for (let i = a; i < b; i++) {
+        result.push(i);
+    }
+    return result;
+};
+
+const calculateBarEntries: (savingsPlan: SavingsPlan) => BarEntry[] = (
+    savingsPlan: SavingsPlan
+) => {
+    const r: number = Math.pow(1 + savingsPlan.interestRate / 100, 1.0 / 12);
+    const a: number = savingsPlan.monthlyInvestment;
+    return range(1, savingsPlan.durationInYears + 1).map((year) => {
+        const months = year * 12;
+        const investment = a * months;
+        return {
+            year: year.toString(),
+            investment,
+            return: a * ((1 - Math.pow(r, 1 + months)) / (1 - r)) - investment,
+        };
+    });
+};
+
+export class CompoundInterestChart extends React.Component<SavingsPlan> {
+    public render() {
+        const data: BarEntry[] = calculateBarEntries(this.props);
+        return (
+            <div>
+                <BarChart
+                    width={600}
+                    height={300}
+                    data={data}
+                    margin={{top: 20, right: 30, left: 20, bottom: 5}}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="year" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+                    <Bar dataKey="return" stackId="b" fill="#82ca9d" />
+                </BarChart>
+            </div>
+        );
+    }
+}
