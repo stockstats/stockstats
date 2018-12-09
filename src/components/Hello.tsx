9d46bf8d6018e2b12505efae7c6f11142277f465
Jannis Mattheis <contact@jmattheis.de>
2018-12-09 12:03:27 +0100

    Format all files

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 const initialState = {
     clickCount: 1,
-    input: "",
+    input: '',
 };
 
 type State = Readonly<typeof initialState>;
@@ -12,22 +12,20 @@ export class HelloState extends React.Component<object, State> {
 
     public render() {
         const {input, clickCount} = this.state;
-        return (<div>
-            <h1>
-                Hello, world!
-            </h1>
-            <p>
-                clickCount: {clickCount}
-            </p>
-            <p>
-                inputNumber: {input}
-            </p>
-            <button onClick={this.modifyCount(x => x + 1)}>Increment!</button>
-            <button onClick={this.modifyCount(x => x - 1)}>Decrement!</button>
-            <input type='number'
-                   value={this.state.input}
-                   onChange={e => this.updateInput(e)}/>
-        </div>);
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <p>clickCount: {clickCount}</p>
+                <p>inputNumber: {input}</p>
+                <button onClick={this.modifyCount((x) => x + 1)}>Increment!</button>
+                <button onClick={this.modifyCount((x) => x - 1)}>Decrement!</button>
+                <input
+                    type="number"
+                    value={this.state.input}
+                    onChange={(e) => this.updateInput(e)}
+                />
+            </div>
+        );
     }
 
     private updateInput(e: React.ChangeEvent<HTMLInputElement>) {
@@ -35,8 +33,9 @@ export class HelloState extends React.Component<object, State> {
             return;
         }
         const value: string = e.target.value;
-        this.setState(s => ({input: value}));
+        this.setState((s) => ({input: value}));
     }
 
-    private modifyCount = (f: (x: number) => number) => () => this.setState(s => ({clickCount: f(s.clickCount)}));
+    private modifyCount = (f: (x: number) => number) => () =>
+        this.setState((s) => ({clickCount: f(s.clickCount)}));
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import {HelloState} from "./components/Hello";
+import {HelloState} from './components/Hello';
 
-ReactDOM.render(
-    <HelloState/>,
-    document.getElementById("example")
-);
+ReactDOM.render(<HelloState />, document.getElementById('example'));
