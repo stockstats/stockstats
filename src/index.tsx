a54532c8a9b12e34e92e88eb4afa046081031a74
Thomas von Rosenberg <vonrosenberg.thomas@gmail.com>
2018-12-10 17:44:25 +0100

    [#2] Adjust homepage title

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import * as ReactDOM from 'react-dom';
 
 import {CompoundInterest} from './components/CompoundInterest';
 
-ReactDOM.render(<CompoundInterest />, document.getElementById('example'));
+ReactDOM.render(<CompoundInterest />, document.getElementById('stockstats'));
